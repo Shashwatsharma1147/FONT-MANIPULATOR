97fd1bff436d6dbabfc9ceef6dcb02e594b1fa4e
Shashwatsharma1147 <76209043+Shashwatsharma1147@users.noreply.github.com>
2022-06-01 17:57:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -13,6 +19,16 @@ function setup(){
 function getPoses(result){
     if(result.length > 0){
         console.log(result);
+        noseX = result[0].pose.nose.x;
+        noseY = result[0].pose.nose.y;
+
+        console.log("noseX = "+noseX+" noseY = "+noseY)
+
+        leftWristX = result[0].pose.leftWrist.x;
+        rightWristX = result[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+        console.log("difference = "+difference);
     }
     else{
         console.log('Error')
@@ -25,4 +41,7 @@ function modelLoaded(){
 
 function draw(){
     background("#34eb49");
+    textSize(difference);
+    fill("purple");
+    text('Shashwat', noseX, noseY)
 }
